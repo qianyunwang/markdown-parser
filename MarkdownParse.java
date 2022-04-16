492b4636f3895280eb4be9aa622f357baee10c7a
qianyunwang <qianyunjean@126.com>
2022-04-15 19:16:23 -0700

    test a new markdown file

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,8 +13,8 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
+            int closeBracket = markdown.indexOf("]", openBracket); 
+            int openParen = markdown.indexOf("(", closeBracket); 
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
---
+++ another-file.md
@@ -0,0 +1,5 @@
+# instructions
+
+
+![image1](image1.png)
+![image2](image2.png)
