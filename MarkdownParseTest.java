d604f3e59b6d04947a9a6ea81c0e732093b99830
qianyunwang <qianyunjean@126.com>
2022-04-24 15:59:14 -0700

    no link
    []

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,25 @@
 import static org.junit.Assert.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import org.junit.*;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testgetlinks() throws IOException{
+        ArrayList<String> test = new ArrayList<>();
+        test.add("https://something.com");
+        test.add("some-thing.html");
+
+        Path fileName = Path.of("/Users/qianyunwang/Documents/GitHub/markdown-parser/test-file.md");
+        String content = Files.readString(fileName);
+
+        assertEquals(test, MarkdownParse.getLinks(content));
     }
 }
--- another-file.md
+++
@@ -1,2 +0,0 @@
-[link1](https://commonmark.org/help/)
-[link2](https://www.youtube.com/watch?v=k67e-Icw4ug)
