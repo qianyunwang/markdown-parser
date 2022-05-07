078a80408676526bf6630349ee85790fcfc7c74b
qianyunwang <qianyunjean@126.com>
2022-05-07 01:12:51 -0700

    adding a comment

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -9,6 +9,7 @@ public class MarkdownParse {
 
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
+        //
         if (markdown.isEmpty()) {
             return toReturn;
         }
